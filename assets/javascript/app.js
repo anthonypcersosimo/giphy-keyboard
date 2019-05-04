4348ba40a7eaadccc08744108d2e93440803438d
Anthony <anthony.cersosimo@gmail.com>
2019-05-04 00:10:38 -0400

    added looper to create buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,8 @@ var btn = $("<button>")
 $( document ).ready(function() {
     console.log( "ready!" );
     for (i = 0; i < startingButtons.length; i++) {
-        
+        var newButton = btn.addClass("dataButton");
+        newButton.text(startingButtons[i]);
+        $("#button-container").prepend(newButton);
     }
 });
