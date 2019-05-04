bd8076bd4c4bf67885af0b984f13c6f1bb63b1ae
Anthony <anthony.cersosimo@gmail.com>
2019-05-03 23:57:22 -0400

    added more CSS style to get a navbar of sorts with borders and a form button and text box for the creation of new buttons, started basic javascript with global variables and a for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 // Javascript goes here!
 
+// Declare global variables
+var startingButtons = ["Spongebob", "Squidward", "Sandy Cheeks", "Patrick Star", "Mr. Krabs", "Plankton", "Mrs. Puff", "Bubble Buddy", "Boo Boo Keys"];
+var btn = $("<button>")
+
 $( document ).ready(function() {
     console.log( "ready!" );
+    for (i = 0; i < startingButtons.length; i++) {
+        
+    }
 });
