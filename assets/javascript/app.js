86b1a9b294f5ecac1ab841c11082c787746042b5
Anthony <anthony.cersosimo@gmail.com>
2019-05-04 10:08:07 -0400

    fixed button looper and commented javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,26 @@
 
 // Declare global variables
 var startingButtons = ["Spongebob", "Squidward", "Sandy Cheeks", "Patrick Star", "Mr. Krabs", "Plankton", "Mrs. Puff", "Bubble Buddy", "Boo Boo Keys"];
-var btn = $("<button>")
 
+// On page load create default buttons
 $( document ).ready(function() {
+    
+    // Console log that the page has loaded
     console.log( "ready!" );
+    // Create loop that will create buttons for each item in 'startingButtons' array
     for (i = 0; i < startingButtons.length; i++) {
-        var newButton = btn.addClass("dataButton");
-        newButton.text(startingButtons[i]);
-        $("#button-container").prepend(newButton);
+        // create new button
+        var btn = $("<button>")
+        // give it a class of gif for styling and listening for click events
+        btn.addClass("gif");
+        // Give the button a specific data attribute that we can grab on click
+        btn.attr("data-name", startingButtons[i]);
+        // Give the button some text so the client can see what they're clicking
+        btn.text(startingButtons[i]);
+        // Append the button to the container that holds the buttons
+        $("#button-container").append(btn);
     }
+
+    // On Click of any button
+    
 });
